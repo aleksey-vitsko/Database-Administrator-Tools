bb0f2477bd64df3c3dcf6a5c106e09884d9a1fcc
aleksey.vitsko <aleksey.vitsko@myib.com>
2025-06-04 14:42:02 -0600

    total rework to support all columns from "sys.dm_db_index_operational_stats" and "sys.dm_db_index_usage_stats"
    total rework to support all columns from "sys.dm_db_index_operational_stats" and "sys.dm_db_index_usage_stats"

--- Tables - ShowTableUsage.sql
+++ Tables - ShowTableUsage.sql
@@ -1,7 +1,7 @@
 
 
 create or alter procedure ShowTableUsage (
-	@DatabaseName			nvarchar(200) = ''
+	@DatabaseName			varchar(200) = ''
 	)
 as begin
 
@@ -9,16 +9,34 @@ as begin
 
 Author: Aleksey Vitsko
 
-Version: 1.01
+Version: 2.00
 
-Description: Shows table usage information (inserts, updates, deletes, locks, etc.) within given database
+Description: 
+
+Shows table usage information (inserts, updates, deletes, scans, seeks, lookups, locks, latches, etc.) for all tables in a specified database.
+For each table (each row), usage statistics is aggregated from all table's indexes.
+
+Useful for finding tables that may not be used (and possibly retiring them for good :D).
+Disclaimer: Keep in mind that usage stats are reset each time server is restarted, so be careful if retiring anything.
+
+Can be called without any parameters - will show tables' usage for current database.
+Can be called specifying a database with @DatabaseName parameter - will show tables' usage for specified database.
 
 --------------------------------------------------------------------------
 
 History:
 
+2025-06-04 --> Aleksey Vitsko - total rework to support all columns from "sys.dm_db_index_operational_stats" and "sys.dm_db_index_usage_stats"
 2024-11-05 --> Aleksey Vitsko - added ability to specify target database name (using the @DatabaseName parameter)
 2024-11-01 --> Aleksey Vitsko - created stored procedure
+
+
+--------------------------------------------------------------------------
+
+Tested on:
+
+- SQL Server 2022
+- Azure SQL Managed Instance (SQL 2022 update policy)
 
 
 *****************************************************************************************************************************************************************/
@@ -38,50 +56,132 @@ History:
 
 	
 	-- execute the query
-	declare @Query nvarchar(max)
+	declare @Query varchar(max)
 
 	set @Query = 'SELECT 
 		s.[name]															[Schema_Name],
 		t.[name]															[Table_Name],
-       
-		sum(leaf_insert_count) + sum(nonleaf_insert_count)				[Inserts],				-- sum of Leaf and Nonleaf inserts
-		sum(leaf_delete_count) +	 sum(iop.nonleaf_delete_count)			[Deletes],				-- sum of Leaf and Nonleaf deletes
-		sum(leaf_update_count) +	 sum(iop.nonleaf_update_count)			[Updates],				-- sum of Leaf and Nonleaf updates
-		sum(leaf_ghost_count)											[Ghosts],				-- sum of Leaf and Nonleaf ghosted records
+		t.[object_id],
+
+		isnull(sum(leaf_insert_count),0) +	isnull(sum(nonleaf_insert_count),0)					[Inserts],				-- sum of Leaf and Nonleaf inserts
+		isnull(sum(leaf_delete_count),0) +	 isnull(sum(iop.nonleaf_delete_count),0)			[Deletes],				-- sum of Leaf and Nonleaf deletes
+		isnull(sum(leaf_update_count),0) +	 isnull(sum(iop.nonleaf_update_count),0)			[Updates],				-- sum of Leaf and Nonleaf updates
+		isnull(sum(leaf_ghost_count),0)												[Ghosts],				-- sum of Leaf and Nonleaf ghosted records
       
-		sum(row_lock_count)												[RowLocks],
-		sum(page_lock_count)												[PageLocks],
+		isnull(sum(range_scan_count),0)												[Range_Scans],
+		isnull(sum(singleton_lookup_count),0)											[Singleton_Lookups],
 
-		sum(range_scan_count)											[Range_Scans],
-		sum(singleton_lookup_count)										[Lookups],
-		sum(forwarded_fetch_count)										[Forwarded_Fetches],
-		sum(lob_fetch_in_pages)											[LOB_Fetches],
-		sum(row_overflow_fetch_in_pages)									[Row_Overflow_Fetches],
-	
-		sum(index_lock_promotion_count)									[Lock_Escalations],
+		isnull(sum(ius.user_seeks),0)													[User_Seeks],
+		isnull(sum(ius.user_scans),0)													[User_Scans],
+		isnull(sum(ius.user_lookups),0)												[User_Lookups],
+		isnull(sum(ius.user_updates),0)												[User_Updates],
 
-		sum(row_lock_count) +  
-		sum(page_lock_count)  + 
-		sum(range_scan_count) + 
-		sum(singleton_lookup_count)										[Locks+Scans+Lookups]
+		isnull(sum(ius.system_seeks),0)												[System_Seeks],
+		isnull(sum(ius.system_scans),0)												[System_Scans],
+		isnull(sum(ius.system_lookups),0)												[System_Lookups],
+		isnull(sum(ius.system_updates),0)												[System_Updates],
 
-	FROM ' + @DatabaseName + '.sys.dm_db_index_operational_stats(DB_ID(''' + @DatabaseName + '''),NULL,NULL,NULL) AS iop
+
+		isnull(sum(row_lock_count),0)													[RowLocks],
+		isnull(sum(page_lock_count),0)												[PageLocks],
+
+		isnull(sum(page_latch_wait_count),0)											[Latch_Waits],
+		isnull(sum(page_io_latch_wait_count),0)										[IO_Latch_Waits],
+
+		isnull(sum(tree_page_latch_wait_count),0)										[Tree_Latch_Waits],
+		isnull(sum(tree_page_io_latch_wait_count),0)									[Tree_IO_Latch_Waits],
+
+		
+
+		isnull(sum(forwarded_fetch_count),0)											[Forwarded_Fetches],
+		isnull(sum(lob_fetch_in_pages),0)												[LOB_Fetches],
+		isnull(sum(lob_orphan_create_count),0)										[LOB_Orphan_Creates],
+		isnull(sum(lob_orphan_insert_count),0)										[LOB_Orphan_Inserts],
+			
+		isnull(sum(column_value_push_off_row_count),0)								[ColVal_Off_Row],
+		isnull(sum(column_value_pull_in_row_count),0)									[ColVal_In_Row],
+		
+		isnull(sum(row_overflow_fetch_in_pages),0)									[Row_Overflow_Fetches],
 	
-		JOIN ' + @DatabaseName + '.sys.indexes AS i 
-			ON iop.index_id = i.index_id 
-			and iop.[object_id] = i.[object_id]
-	
-		JOIN ' + @DatabaseName + '.sys.tables AS t ON 
-			i.[object_id] = t.[object_id] 
-	
-		join ' + @DatabaseName + '.sys.schemas s on
+		isnull(sum(index_lock_promotion_attempt_count),0)								[Escalation_Attempts],
+		isnull(sum(index_lock_promotion_count),0)										[Lock_Escalations],
+
+		isnull(sum(page_compression_attempt_count),0)									[Page_Compression_Attempts],
+		isnull(sum(page_compression_success_count),0)									[Page_Compression_Success],
+
+		isnull(sum(version_generated_inrow),0)										[Version_Generated_In_Row],
+		isnull(sum(version_generated_offrow),0)										[Version_Generated_Off_Row],
+
+		isnull(sum(ghost_version_inrow),0)											[Ghost_Version_In_Row],
+		isnull(sum(ghost_version_offrow),0)											[Ghost_Version_Off_Row],
+
+		isnull(sum(insert_over_ghost_version_inrow),0)								[Insert_Over_Ghost_In_Row],
+		isnull(sum(insert_over_ghost_version_offrow),0)								[Insert_Over_Ghost_Off_Row],
+
+		
+		isnull(sum(leaf_insert_count),0) +	
+		isnull(sum(nonleaf_insert_count),0) + 
+		isnull(sum(leaf_delete_count),0) +	 
+		isnull(sum(iop.nonleaf_delete_count),0) +
+		isnull(sum(leaf_update_count),0) +	 
+		isnull(sum(iop.nonleaf_update_count),0) +
+		isnull(sum(leaf_ghost_count),0)	+
+		isnull(sum(range_scan_count),0) +
+		isnull(sum(singleton_lookup_count),0) +
+		isnull(sum(ius.user_seeks),0) +
+		isnull(sum(ius.user_scans),0) +
+		isnull(sum(ius.user_lookups),0) +
+		isnull(sum(ius.user_updates),0) +
+		isnull(sum(ius.system_seeks),0) +
+		isnull(sum(ius.system_scans),0) +
+		isnull(sum(ius.system_lookups),0) +
+		isnull(sum(ius.system_updates),0) +
+		isnull(sum(row_lock_count),0) +
+		isnull(sum(page_lock_count),0) +
+		isnull(sum(page_latch_wait_count),0) +
+		isnull(sum(page_io_latch_wait_count),0) +
+		isnull(sum(tree_page_latch_wait_count),0) +
+		isnull(sum(tree_page_io_latch_wait_count),0) +
+		isnull(sum(forwarded_fetch_count),0) +
+		isnull(sum(lob_fetch_in_pages),0) +
+		isnull(sum(lob_orphan_create_count),0) +
+		isnull(sum(lob_orphan_insert_count),0) +
+		isnull(sum(column_value_push_off_row_count),0) +
+		isnull(sum(column_value_pull_in_row_count),0) +
+		isnull(sum(row_overflow_fetch_in_pages),0) +
+		isnull(sum(index_lock_promotion_attempt_count),0) +
+		isnull(sum(index_lock_promotion_count),0) +
+		isnull(sum(page_compression_attempt_count),0) +
+		isnull(sum(page_compression_success_count),0) +
+		isnull(sum(version_generated_inrow),0) +
+		isnull(sum(version_generated_offrow),0)	 +
+		isnull(sum(ghost_version_inrow),0) +
+		isnull(sum(ghost_version_offrow),0) +
+		isnull(sum(insert_over_ghost_version_inrow),0) +
+		isnull(sum(insert_over_ghost_version_offrow),0)				[Sum_Usage]
+		
+
+	FROM ' + @DatabaseName + '.sys.tables t
+
+		left join ' + @DatabaseName + '.sys.schemas s on
 			t.[schema_id] = s.[schema_id]
+	
+		left join ' + @DatabaseName + '.sys.dm_db_index_operational_stats(DB_ID(''' + @DatabaseName + '''),NULL,NULL,NULL) iop on
+			t.[object_id] = iop.[object_id]
 
-	GROUP BY s.[name], t.[name]
-	ORDER BY [Locks+Scans+Lookups] desc, s.[name], t.[name]'
+		left join ' + @DatabaseName + '.sys.indexes i on 
+			t.[object_id]  = i.[object_id]
+			and iop.index_id = i.index_id 
+	
+		left join ' + @DatabaseName + '.sys.dm_db_index_usage_stats ius on
+			t.[object_id] = ius.[object_id]
+
+	GROUP BY s.[name], t.[name], t.[object_id]
+	ORDER BY [Sum_Usage] desc, s.[name], t.[name]'
 
 
 	--print @Query
+	-- print len(@Query)
 
 	exec sp_executesql @stmt = @Query
 
