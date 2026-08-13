600d6cd228946d265997f03014e79a239462cf52
Aleksey Vitsko <59313042+aleksey-vitsko@users.noreply.github.com>
2026-08-13 16:22:46 -0600

    MemoryManagerInfo - collect and show system-level memory specs in Expert Mode
    collect and show system-level memory specs in Expert Mode

--- MemoryManagerInfo.sql
+++ MemoryManagerInfo.sql
@@ -11,18 +11,19 @@ set nocount on
 
 Author: Aleksey Vitsko
 
-Version: 1.11
+Version: 1.12
 
 Purpose: shows current memory usage 
 (how much RAM memory is being used by the Database Engine, and what for: database cache, plan cache, memory grants, connections, locks, etc.)
 
 History:
 
+2026-08-13 --> Aleksey Vitsko - collect and show system-level memory specs in Expert Mode
 2025-03-27 --> Aleksey Vitsko - show all memory clerks in Expert mode (to include SQLBUFFERPOOL)
 2024-03-15 --> Aleksey Vitsko - percentage for "Maximum Workspace Memory" should be calculated in relation to "Target Server Memory", not "Total Server Memory" (issue https://github.com/aleksey-vitsko/Database-Administrator-Tools/issues/3)
 2024-02-16 --> Aleksey Vitsko - make SP show memory counters information on Azure SQL Managed Instance and Azure SQL DB
 2022-09-06 --> Aleksey Vitsko - slight updates to perf.counter description texts
-2022-09-06 --> Aleksey Vitsko - added information from "sys.dm_os_memory_clerks" to output in the Expert Mode = 1
+2022-09-06 --> Aleksey Vitsko - added information from "sys.dm_os_memory_clerks" to the output in Expert Mode
 2022-08-27 --> Aleksey Vitsko - added @ExpertMode parameter - 0 is default and is simpler output, 1 will show all columns and details
 2022-08-26 --> Aleksey Vitsko - added "counter_location" column to the output
 2022-08-26 --> Aleksey Vitsko - added "percentage_info" column to the output
@@ -189,6 +190,70 @@ end
 
 
 if @ExpertMode = 1 begin
+
+	
+	declare 
+		@EngineEdition	int
+
+	set @EngineEdition = cast((select Serverproperty('EngineEdition')) as int)
+
+
+	if @EngineEdition not in (5) begin
+
+		/* collect system related memory specs */
+		declare 
+			@TotalPhysicalMemoryGB				decimal(32,2),
+			@AvailablePhysicalMemoryGB			decimal(32,2),
+			@PageFileGB							decimal(32,2),
+			@AvailablePageFileGB				decimal(32,2),
+			@SystemCacheGB						decimal(32,2),
+			@KernelPagedPoolGB					decimal(32,2),
+			@KernelNonPagedPoolGB				decimal(32,2),
+			@SystemMemoryState					nvarchar(256),
+			@ProcessMemoryLimitGB				decimal(32,2)
+
+		select 
+			@TotalPhysicalMemoryGB = cast(cast(total_physical_memory_kb as decimal(32,2)) / 1024 / 1024	 as decimal(32,2)),
+			@AvailablePhysicalMemoryGB = cast(cast(available_physical_memory_kb as decimal(32,2)) / 1024 / 1024	 as decimal(32,2)),
+			@PageFileGB = cast(cast(total_page_file_kb as decimal(32,2)) / 1024 / 1024 as decimal(32,2)),
+			@AvailablePageFileGB = cast(cast(available_page_file_kb as decimal(32,2)) / 1024 / 1024 as decimal(32,2)),
+			@SystemCacheGB = cast(cast(system_cache_kb as decimal(32,2)) / 1024 / 1024 as decimal(32,2)),
+			@KernelPagedPoolGB = cast(cast(kernel_paged_pool_kb as decimal(32,2)) / 1024 / 1024 as decimal(32,2)),
+			@KernelNonPagedPoolGB = cast(cast(kernel_nonpaged_pool_kb as decimal(32,2)) / 1024 / 1024 as decimal(32,2)),
+			@SystemMemoryState = system_memory_state_desc
+		from sys.dm_os_sys_memory
+
+
+		drop table if exists #SysMemory
+
+		create table #SysMemory (
+			ID						int identity primary key,
+			sys_memory				varchar(128),
+			sys_memory_location		varchar(128),
+			sys_memory_desc			varchar(256),
+			[GB]					varchar(128)
+			)
+
+		insert into #SysMemory (sys_memory, sys_memory_location, sys_memory_desc, [GB])
+		values	('Total Physical Memory','sys.dm_os_sys_memory','Total size of physical memory available to the operating system',cast(@TotalPhysicalMemoryGB as varchar(128))),
+				('Available Physical Memory','sys.dm_os_sys_memory','Size of physical memory available',cast(@AvailablePhysicalMemoryGB as varchar(128))),
+				('Page File','sys.dm_os_sys_memory','Size of the commit limit reported by the operating system',cast(@PageFileGB as varchar(128))),
+				('Available Page File','sys.dm_os_sys_memory','Total amount of page file that is not being used',cast(@AvailablePageFileGB as varchar(128))),
+				('System Cache','sys.dm_os_sys_memory','Total amount of system cache memory',cast(@SystemCacheGB as varchar(128))),
+				('Kernel Paged Pool','sys.dm_os_sys_memory','Total amount of the paged kernel pool',cast(@KernelPagedPoolGB as varchar(128))),
+				('Kernel Non PagedPool','sys.dm_os_sys_memory','Total amount of the nonpaged kernel pool',cast(@KernelNonPagedPoolGB as varchar(128))),
+				('System Memory State','sys.dm_os_sys_memory','Description of the memory state',@SystemMemoryState)
+
+
+		select
+			sys_memory,
+			sys_memory_location,
+			sys_memory_desc,
+			[GB]
+		from #SysMemory
+
+	end
+
 
 	-- show break down of memory usage by SQL Server
 	select
