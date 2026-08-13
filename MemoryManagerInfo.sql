6cd7d753fc2d967b160e1954c908cb9394774370
Aleksey Vitsko <59313042+aleksey-vitsko@users.noreply.github.com>
2026-08-13 16:58:55 -0600

    formatting changes, "tested on" section, updated description
    formatting changes, "tested on" section, updated description

--- MemoryManagerInfo.sql
+++ MemoryManagerInfo.sql
@@ -4,63 +4,80 @@
 
 
 create or alter procedure MemoryManagerInfo (
-	@ExpertMode			tinyint = 0)
+	@ExpertMode			tinyint = 0
+	)
 
 as begin	
 
 set nocount on
 
-/***************************************************MEMORY MANAGER INFO PROCEDURE ********************************************************
+/*************************************************** MEMORY MANAGER INFO PROCEDURE ********************************************************
 
 Author: Aleksey Vitsko
 
-Version: 1.12
+Version: 1.14
 
-Purpose: shows current memory usage 
-(how much RAM memory is being used by the Database Engine, and what for: database cache, plan cache, memory grants, connections, locks, etc.)
+
+Description: 
+
+Shows details for current memory usage by database engine and host system
+
 
 History:
 
+2026-08-13 - Aleksey Vitsko - added "tested on... section", updated description
+2026-08-13 - Aleksey Vitsko - formatting changes, comments using /* */
 2026-08-13 - Aleksey Vitsko - added "process memory limit" for Azure SQL Database
-2026-08-13 --> Aleksey Vitsko - collect and show system-level memory specs in Expert Mode
-2025-03-27 --> Aleksey Vitsko - show all memory clerks in Expert mode (to include SQLBUFFERPOOL)
-2024-03-15 --> Aleksey Vitsko - percentage for "Maximum Workspace Memory" should be calculated in relation to "Target Server Memory", not "Total Server Memory" (issue https://github.com/aleksey-vitsko/Database-Administrator-Tools/issues/3)
-2024-02-16 --> Aleksey Vitsko - make SP show memory counters information on Azure SQL Managed Instance and Azure SQL DB
-2022-09-06 --> Aleksey Vitsko - slight updates to perf.counter description texts
-2022-09-06 --> Aleksey Vitsko - added information from "sys.dm_os_memory_clerks" to the output in Expert Mode
-2022-08-27 --> Aleksey Vitsko - added @ExpertMode parameter - 0 is default and is simpler output, 1 will show all columns and details
-2022-08-26 --> Aleksey Vitsko - added "counter_location" column to the output
-2022-08-26 --> Aleksey Vitsko - added "percentage_info" column to the output
-2022-08-26 --> Aleksey Vitsko - corrections to perf.counter descriptions to make them more understandable
-2022-08-26 --> Aleksey Vitsko - added "Server Total RAM" to the output
-2022-06-10 --> Aleksey Vitsko - lets have "cntr_value_GB" show decimal values, too
-2019-11-05 --> Aleksey Vitsko - added "Plan Cache" memory info
-2019-11-01 --> Aleksey Vitsko - created procedure
+2026-08-13 - Aleksey Vitsko - collect and show system-level memory specs in Expert Mode
+
+2025-03-27 - Aleksey Vitsko - show all memory clerks in Expert mode (to include SQLBUFFERPOOL)
+
+2024-03-15 - Aleksey Vitsko - percentage for "Maximum Workspace Memory" should be calculated in relation to "Target Server Memory", not "Total Server Memory" (issue https://github.com/aleksey-vitsko/Database-Administrator-Tools/issues/3)
+2024-02-16 - Aleksey Vitsko - make SP show memory counters information on Azure SQL Managed Instance and Azure SQL DB
+
+2022-09-06 - Aleksey Vitsko - slight updates to perf.counter description texts
+2022-09-06 - Aleksey Vitsko - added information from "sys.dm_os_memory_clerks" to the output in Expert Mode
+2022-08-27 - Aleksey Vitsko - added @ExpertMode parameter - 0 is default and is simpler output, 1 will show all columns and details
+2022-08-26 - Aleksey Vitsko - added "counter_location" column to the output
+2022-08-26 - Aleksey Vitsko - added "percentage_info" column to the output
+2022-08-26 - Aleksey Vitsko - corrections to perf.counter descriptions to make them more understandable
+2022-08-26 - Aleksey Vitsko - added "Server Total RAM" to the output
+2022-06-10 - Aleksey Vitsko - lets have "cntr_value_GB" show decimal values, too
+
+2019-11-05 - Aleksey Vitsko - added "Plan Cache" memory info
+2019-11-01 - Aleksey Vitsko - created procedure
+
+
+Tested on:
+
+- SQL Server 2016 (SP2), 2017 (RTM), 2019 (RTM), 2022 (RTM), 2025 (RTM)
+- Azure SQL Managed Instance (SQL 2022 update policy)
+- Azure SQL Database
+
 
 ***************************************************************************************************************************************/
 
 
-
--- @ExpertMode validation
+/* @ExpertMode validation */
 if @ExpertMode not in (0,1) begin
 	print '@ExpertMode can only be 0 or 1'
 	return
 end
 
 
--- use temp table
+/* use temp table */
 if object_id('TempDB..#MemoryManager') is not NULL begin drop table #MemoryManager end
 
 create table #MemoryManager (
 	ID					int identity primary key,
 	
-	counter_name		varchar(100),			-- PerfMon counter name
+	counter_name		varchar(100),			/* PerfMon counter name */
 	counter_location	varchar(100),
-	counter_desc		varchar(500),			-- counter description
+	counter_desc		varchar(500),			/* counter description */
 	
-	cntr_value			bigint,					-- value in kilobytes
-	cntr_value_MB		bigint,					-- value in megabytes
-	cntr_value_GB		decimal(8,2),			-- value in gigabytes
+	cntr_value			bigint,					/* value in kilobytes */
+	cntr_value_MB		bigint,					/* value in megabytes */
+	cntr_value_GB		decimal(8,2),			/* value in gigabytes */
 
 	pct					decimal(5,2),
 	percentage_info		varchar(100)
@@ -68,7 +85,7 @@ create table #MemoryManager (
 	)
 
 
--- insert memory manager performance counter names with descriptions
+/* insert memory manager performance counter names with descriptions */
 insert into #MemoryManager (counter_name, counter_location, counter_desc)
 values	('Total Server Memory','sys.dm_os_performance_counters - SQLServer:Memory Manager','Total amount of memory SQL Server is currently consuming ("Database Cache Memory" + "Stolen Server Memory" + "Free Memory")'),
 		('Target Server Memory','sys.dm_os_performance_counters - SQLServer:Memory Manager','Total amount of memory SQL Server is allowed to consume (can be limited by "max server memory" setting)'),
@@ -86,7 +103,7 @@ values	('Total Server Memory','sys.dm_os_performance_counters - SQLServer:Memory
 		('Plan Cache','sys.dm_exec_cached_plans (size_in_bytes)','Amount of memory used for cached query execution plans')
 
 
--- get memory manager performance counters current values
+/* get memory manager performance counters current values */
 update m
 	set m.cntr_value = pc.cntr_value
 from #MemoryManager m
@@ -102,7 +119,7 @@ where	counter_name = 'Plan Cache'
 
 
 
--- total RAM on a server
+/* total RAM on a server */
 declare @Total_RAM_on_a_server bigint
 set @Total_RAM_on_a_server = (select physical_memory_kb from sys.dm_os_sys_info)
 
@@ -111,7 +128,7 @@ values ('Server Total RAM','sys.dm_os_sys_info (physical_memory_kb)','Total amou
 
 
 
--- calculate values in megabytes and gigabytes
+/* calculate values in megabytes and gigabytes */
 update #MemoryManager
 	set cntr_value_MB = cntr_value / 1024,
 		cntr_value_GB = cast(cntr_value as decimal(16,2)) / 1024 / 1024
@@ -119,7 +136,7 @@ update #MemoryManager
 
 
 
--- percentages calculation
+/* percentages calculation */
 if @ExpertMode = 1 begin
 
 	declare 
@@ -136,7 +153,7 @@ if @ExpertMode = 1 begin
 	where	counter_name = 'Server Total RAM'
 
 
-	-- target server memory 
+	/* target server memory */
 	update #MemoryManager
 		set pct = (cast(@Target_Server_Memory as decimal(16,2)) / cast(@Total_RAM_on_a_server as decimal(16,2))) * 100
 	where	counter_name = 'Target Server Memory'
@@ -146,7 +163,7 @@ if @ExpertMode = 1 begin
 	where	counter_name = 'Target Server Memory'
 
 
-	-- total server memory
+	/* total server memory */
 	update #MemoryManager
 		set pct = (cast(@Total_SQL_Server_Memory as decimal(16,2)) / cast(@Target_Server_Memory as decimal(16,2))) * 100
 	where	counter_name = 'Total Server Memory'
@@ -156,7 +173,7 @@ if @ExpertMode = 1 begin
 	where	counter_name = 'Total Server Memory'
 
 
-	-- maximum workspace memory
+	/* maximum workspace memory */
 	update #MemoryManager
 		set pct = (cast(cntr_value as decimal(16,2)) / cast(@Target_Server_Memory as decimal(16,2))) * 100
 	where	counter_name = 'Maximum Workspace Memory'
@@ -166,7 +183,7 @@ if @ExpertMode = 1 begin
 	where	counter_name = 'Maximum Workspace Memory'
 
 	   
-	-- other counters
+	/* other counters */
 	update #MemoryManager
 		set pct = (cast(cntr_value as decimal(16,2)) / cast(@Total_SQL_Server_Memory as decimal(16,2))) * 100
 	where pct is NULL
@@ -179,7 +196,7 @@ end
 
 
 
--- show collected data
+/* show collected data */
 if @ExpertMode = 0 begin
 
 	select
@@ -271,7 +288,7 @@ if @ExpertMode = 1 begin
 	end
 
 
-	-- show break down of memory usage by SQL Server
+	/* show break down of memory usage by SQL Server  */
 	select
 		counter_name,
 		counter_location,
@@ -284,7 +301,7 @@ if @ExpertMode = 1 begin
 	order by cntr_value_MB desc
 
 
-	-- show memory clerks 
+	/* show memory clerks */ 
 	select 
 		[type]								[clerk_name], 
 		case [type]
